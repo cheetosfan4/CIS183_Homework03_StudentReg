489480a11229c442a5a111e57af029b6342a8035
cheetosfan4 <146988149+cheetosfan4@users.noreply.github.com>
2025-10-28 05:29:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cis183_homework03_studentreg/StudentDetailsActivity.java
@@ -0,0 +1,26 @@
+package com.example.cis183_homework03_studentreg;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StudentDetailsActivity extends AppCompatActivity {
+    //for viewing and editing the details of a specific student
+    //admin cannot edit primary key
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_student_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
